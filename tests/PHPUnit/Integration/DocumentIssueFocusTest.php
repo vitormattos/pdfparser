56d28b9302c8e7d6782328e83e9744fad434d4e8
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-24 13:24:57 -0300

    fix: parse xref tables with interleaved comment lines
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
    (cherry picked from commit 3e2a9c66d796c2fa707894a95f4dbea2677244ac)

--- tests/PHPUnit/Integration/DocumentIssueFocusTest.php
+++ tests/PHPUnit/Integration/DocumentIssueFocusTest.php
@@ -184,6 +184,13 @@ class DocumentIssueFocusTest extends TestCase
         self::assertCount(1, $document->getPages());
     }
 
+    public function testParseFileWithCommentsInsideXrefTable(): void
+    {
+        $document = (new Parser())->parseFile($this->rootDir.'/samples/bugs/PullRequest805-pdf.js.pdf');
+
+        self::assertCount(3, $document->getPages());
+    }
+
     /**
      * @group linux-only
      */
