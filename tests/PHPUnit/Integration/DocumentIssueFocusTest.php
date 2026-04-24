e45ded8ddc939ae2d8faaf4512235b5d92db9d4c
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-24 15:33:19 -0300

    fix: recover malformed xref table in issue9252
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
    (cherry picked from commit 35130403aec0850338d1a94103a1f092614f893e)

--- tests/PHPUnit/Integration/DocumentIssueFocusTest.php
+++ tests/PHPUnit/Integration/DocumentIssueFocusTest.php
@@ -112,6 +112,12 @@ class DocumentIssueFocusTest extends TestCase
         $testSubject = '•†‡…—–ƒ⁄‹›−‰„“”‘’‚™ŁŒŠŸŽıłœšž';
         self::assertStringContainsString($testSubject, $details['Subject']);
     }
+    public function testParseFileWithXrefTableMissingXrefKeyword(): void
+    {
+        $document = (new Parser())->parseFile($this->rootDir.'/samples/bugs/PullRequest807-pdfjs-xref-missing-keyword.pdf');
+
+        self::assertCount(1, $document->getPages());
+    }
 
     /**
      * @see https://github.com/smalot/pdfparser/pull/795
